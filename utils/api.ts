87b10cc921b6559c41eef9bea3dbab4ea8757998
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-03-16 15:49:06 +0800

    backend configuration: done with utils

--- utils/api.ts
+++ utils/api.ts
@@ -1,21 +1,43 @@
-
 // Mock API utility
+
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:8000/api";
+
 const api = {
     post: async (endpoint: string, data: any) => {
-        console.log(`POST request to ${endpoint}`, data);
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve({ data: { success: true, token: "mock-token" } });
-            }, 1000);
-        });
+        console.log(`POST request to ${BASE_URL}${endpoint}`, data);
+        
+        try {
+            const res = await fetch(`${BASE_URL}${endpoint}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json' 
+                },
+                body: JSON.stringify(data)
+            });
+            const result = await res.json();
+            return {data: result, status: res.status};
+        } catch (error) {
+            console.error(`Error occurred while making POST request to ${BASE_URL}${endpoint}:`, error);
+            throw error;
+        }
     },
     get: async (endpoint: string) => {
-        console.log(`GET request to ${endpoint}`);
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve({ data: { success: true } });
-            }, 500);
-        });
+        console.log(`GET request to ${BASE_URL}${endpoint}`);
+        try {
+            const res = await fetch(`${BASE_URL}${endpoint}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+            const result = await res.json();
+            return { data: result, status: res.status };
+
+        } catch (error) {
+            console.error(`Error occurred while making GET request to ${BASE_URL}${endpoint}:`, error);
+            throw error;
+        }
     }
 };
 
